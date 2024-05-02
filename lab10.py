1c836d190bebd2fb750f911204db15bf184083d1
divyajmk kuruvilla <yaq7fm@virginia.edu>
2024-05-02 03:41:58 +0000

    finished lab10

--- lab10.py
+++ lab10.py
@@ -15,7 +15,7 @@ def new_throw_me_an_error():
 new_throw_me_an_error
 
 # 2. Error Handling - finally
-# The finally clause executes as the last task before the try statement completes. 
+# The finally clause serves as the last clause before the try statement completes. 
 # The finally clause runs whether or not the try statement produces an exception.
 # In this function, the finally clause serves to ensure that the file is properly closed after the write 
 # operation is completed, regardless of whether an exception occured or not which is important as it ensures 
@@ -23,7 +23,7 @@ new_throw_me_an_error
 # This finally clause is important as it ensures the file is closed once it is not needed anymore,
 # and this helps prevent the file from being open and allowing other programs to access the file.
 # If an exception occurs within the try block when the file is trying to be opened or written to, then 
-# the try statement will stop executing, and the program will move to except block to handle the exception. 
+# the try statement will stop executing, and the program will move to the except block to handle the exception. 
 # After the except block, the finally block is executed.
 
 # 3. Error Handing - Imported Library 
@@ -35,7 +35,8 @@ data = '{"invalid_json_key": "value"}'
 
 try:
   # Attempt to load the JSON data
-  json.loads(data)
+  d = json.loads(data)
+  print(d)
 except json.JSONDecodeError as e:
   # Print the JSON import error
   print(f"JSON import error: {e}")
